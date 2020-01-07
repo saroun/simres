2fc096c3012ab937b073b2b6426f228eee148f56
Jan Saroun <jsaroun@seznam.cz>
2020-01-07 16:27:55 +0100

    updates simres-mcstas demo scripts, adds user info

--- resources/demo/simres_mcstas/mcstas/compile.bat
+++ resources/demo/simres_mcstas/mcstas/compile.bat
@@ -1,20 +1,22 @@
 @echo off
+rem set up environment for your McStas distribution
 set PATH=C:/mcstas-2.5/bin;%PATH%
 set MCSTAS=C:/mcstas-2.5/lib
 set MCSTAS_CC=gcc
 set MCSTAS_TOOLS=C:/mcstas-2.5/lib/tools/Perl
-rem 
+
 set name=%1
 if [%name%]==[] (
 	echo Provide file name without extension as a parameter.
-	goto end
+	echo Trying BEER_MCPL_PWD
+	set name=BEER_MCPL_PWD
 )
 echo Compiling %name%
 
 del %name%.c
 mcstas -o %name%.c %name%.instr
 %MCSTAS_CC% -o %name%.exe %name%.c -I %MCSTAS%/libs/mcpl %MCSTAS%/libs/mcpl/libmcpl.a
-
+del /Q %name%.c 
 
 :end
 
--- resources/demo/simres_mcstas/mcstas/compile.sh
+++ resources/demo/simres_mcstas/mcstas/compile.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# set up environment for your McStas distribution
 MCSTAS=/usr/share/mcstas/2.5
 MCSTAS_CC=gcc
-
+INS=$1
 if [ -z "$1" ]
 then
 	echo Provide file name without extension as a parameter.
-else
-	echo Compiling $1
-	mcstas -o $1.c $1.instr
-	$MCSTAS_CC -o $1.exe $1.c -lm -I $MCSTAS/libs/mcpl $MCSTAS/libs/mcpl/libmcpl.a
+	echo Trying BEER_MCPL_PWD.
+	INS=BEER_MCPL_PWD
 fi
-
+else
+   INS=$1
+fi
+echo Compiling $INS
+mcstas -o $INS.c $INS.instr
+$MCSTAS_CC -o $INS.exe $INS.c -lm -I $MCSTAS/libs/mcpl $MCSTAS/libs/mcpl/libmcpl.a
+rm $INS.c
 
